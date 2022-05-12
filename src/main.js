d2ee701e5e5535d09791dc877e111937b3495bc0
Diana Martinez <diananerdoficial@gmail.com>
2022-05-12 13:58:02 -0500

    Setup pinia

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './index.css'
@@ -6,7 +7,10 @@ import { Icon } from '@iconify/vue';
 
 const app = createApp(App)
 
+const pinia = createPinia()
+
 app.use(router)
+app.use(pinia)
 app.component("Icon", Icon)
 
 app.mount('#app')
